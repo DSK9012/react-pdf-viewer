82e79ab5d9ae7d3fe31e6bd11f87bbe34a721386
Sai Kiran Dannana <saikiran.dannana@gmail.com>
2024-01-27 23:19:24 +0530

    state issue in search plugin

--- demo/nextjs/pages/search-sidebar/index.tsx
+++ demo/nextjs/pages/search-sidebar/index.tsx
@@ -16,29 +16,33 @@ const SearchSidebar: React.FC<{
 
 const IndexPage = () => {
     const searchPluginInstance = searchPlugin();
+    const [count, setCunt] = React.useState(0);
 
     return (
-        <div
-            style={{
-                border: '1px solid rgba(0, 0, 0, .3)',
-                display: 'flex',
-                height: '50rem',
-                margin: '5rem auto',
-                width: '64rem',
-            }}
-        >
+        <div style={{ width: '64rem', margin: '5rem auto' }}>
+            <button onClick={() => setCunt((prevState) => prevState + 1)}>Counter : {count}</button>
+            <br />
+            <br />
             <div
                 style={{
-                    borderRight: '1px solid rgba(0, 0, 0, .2)',
-                    flex: '0 0 15rem',
-                    width: '15rem',
+                    border: '1px solid rgba(0, 0, 0, .3)',
+                    display: 'flex',
+                    height: '50rem',
                 }}
             >
-                <SearchSidebar searchPluginInstance={searchPluginInstance} />
-            </div>
+                <div
+                    style={{
+                        borderRight: '1px solid rgba(0, 0, 0, .2)',
+                        flex: '0 0 15rem',
+                        width: '15rem',
+                    }}
+                >
+                    <SearchSidebar searchPluginInstance={searchPluginInstance} />
+                </div>
 
-            <div style={{ flex: 1 }}>
-                <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[searchPluginInstance]} />
+                <div style={{ flex: 1 }}>
+                    <Viewer fileUrl="/pdf-open-parameters.pdf" plugins={[searchPluginInstance]} />
+                </div>
             </div>
         </div>
     );
